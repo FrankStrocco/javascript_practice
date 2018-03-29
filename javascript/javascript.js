481c06e8bce4625863045940f4f491c5a47a9a69
FrankStrocco <frank.strocco@gmail.com>
2018-03-29 16:14:11 -0700

    worked through some basic javascript again

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,55 @@
-console.log("it's up and running");
+//simple funcitons
+
+console.log("up and running");
+
+
+// (function() {
+//   alert("yo yo");
+// }())
+
+// Returns from functions
+function findBiggestFraction(a, b) {
+  let result;
+  return a > b ? result = ["firstFraction", a] : result = ["secondFraction", b]
+
+}
+
+var firstFraction = 3/4;
+var secondFraction = 5/7;
+
+var biggestFraction = findBiggestFraction(firstFraction, secondFraction);
+console.log("firstFraction is ", firstFraction);
+console.log("secondFraction is ", secondFraction);
+console.log(`Fraction named, "${biggestFraction[0]}" has the largest value which is: ${biggestFraction[1]}.`);
+
+
+//Anonymous functions
+
+var a = 3/4;
+var b = 5/7;
+
+var theBiggest = function(a, b){
+  var result;
+  a > b ? result = ["a", a] : result = ["b", b];
+  return result;
+}
+
+console.log(theBiggest(7/9, 13/25));
+
+
+// immediately invoked functions
+
+
+
+var a = 3/4;
+var b = 5/7;
+
+
+
+var theBiggest = (function(a, b){
+  var result;
+  a > b ? result = ["a", a] : result = ["b", b];
+  return result;
+})(firstFraction, secondFraction);
+
+console.log("theBiggest", theBiggest);
