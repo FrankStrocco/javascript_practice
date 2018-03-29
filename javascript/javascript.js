35133b8837614a0b8bf30f23c076da8a8de23ff0
FrankStrocco <frank.strocco@gmail.com>
2018-03-29 16:55:36 -0700

    commit before branching

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,6 @@
 //simple funcitons
 
-console.log("up and running");
+// console.log("up and running");
 
 
 // (function() {
@@ -8,48 +8,93 @@ console.log("up and running");
 // }())
 
 // Returns from functions
-function findBiggestFraction(a, b) {
-  let result;
-  return a > b ? result = ["firstFraction", a] : result = ["secondFraction", b]
-
-}
-
-var firstFraction = 3/4;
-var secondFraction = 5/7;
-
-var biggestFraction = findBiggestFraction(firstFraction, secondFraction);
-console.log("firstFraction is ", firstFraction);
-console.log("secondFraction is ", secondFraction);
-console.log(`Fraction named, "${biggestFraction[0]}" has the largest value which is: ${biggestFraction[1]}.`);
+// function findBiggestFraction(a, b) {
+//   let result;
+//   return a > b ? result = ["firstFraction", a] : result = ["secondFraction", b]
+//
+// }
+//
+// var firstFraction = 3/4;
+// var secondFraction = 5/7;
+//
+// var biggestFraction = findBiggestFraction(firstFraction, secondFraction);
+// console.log("firstFraction is ", firstFraction);
+// console.log("secondFraction is ", secondFraction);
+// console.log(`Fraction named, "${biggestFraction[0]}" has the largest value which is: ${biggestFraction[1]}.`);
 
 
 //Anonymous functions
 
-var a = 3/4;
-var b = 5/7;
-
-var theBiggest = function(a, b){
-  var result;
-  a > b ? result = ["a", a] : result = ["b", b];
-  return result;
-}
-
-console.log(theBiggest(7/9, 13/25));
+// var a = 3/4;
+// var b = 5/7;
+//
+// var theBiggest = function(a, b){
+//   var result;
+//   a > b ? result = ["a", a] : result = ["b", b];
+//   return result;
+// }
+//
+// console.log(theBiggest(7/9, 13/25));
 
 
 // immediately invoked functions
 
 
 
-var a = 3/4;
-var b = 5/7;
+// var a = 3/4;
+// var b = 5/7;
 
 
 
-var theBiggest = (function(a, b){
-  var result;
-  a > b ? result = ["a", a] : result = ["b", b];
-  return result;
-})(firstFraction, secondFraction);
+// var theBiggest = (function(a, b){
+//   var result;
+//   a > b ? result = ["a", a] : result = ["b", b];
+//   return result;
+// })(firstFraction, secondFraction);
+//
+// console.log("theBiggest", theBiggest);
 
-console.log("theBiggest", theBiggest);
+// objects
+
+
+var course = new Object();
+//
+// course.title = "JavaScript Essential Training";
+// cousr...
+
+var course = {
+  title: "JavaScript Essential Training",
+  instructor: "Frank Strocco",
+  level: 1,
+  published: true,
+  views: 0
+}
+
+console.log(course);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
